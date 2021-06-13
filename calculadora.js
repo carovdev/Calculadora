30d2e18eda7682ebf24e29c556dcc70c8ac078bb
Carolina-Vega-fs <carolinaangelesvega@hotmail.com>
2021-06-12 21:26:15 -0300

    mi primer commit

---
+++ calculadora.js
@@ -0,0 +1,15 @@
+const suma = require('./sumar.js')
+console.log(suma(30,14));
+
+
+const resta = require('./restar.js')
+console.log(resta(20,7));
+
+
+const multiplicacion = require('./multiplicar')
+console.log(multiplicacion(12,15));
+
+
+const division = require('./dividir.js')
+console.log(division(0,3));
+console.log(division(20,2));
